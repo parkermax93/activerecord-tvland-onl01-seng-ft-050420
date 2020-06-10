fdbdccb43182d0492c6ee88654bfe9f8228bccc9
Parker Williamson <parker.williamson@gmail.com>
2020-06-10 20:52:37 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
-    create_table actors do 
-    add_column :actors, :first_name, :string 
+    create_table :actors do |t|
+    t.string :first_name
     add_column :actors, :last_name, :string
   end
 end
