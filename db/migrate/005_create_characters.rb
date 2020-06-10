46e97fc2d17b9ab795a6a933f05e4fc14de75c36
Parker Williamson <parker.williamson@gmail.com>
2020-06-10 20:53:41 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    add_column :characters, :name, :string 
-    add_column :characters, :actor_id, :integer
+    create_table :characters do |t|
+    t.string :name
+    t.string :actor_id
     add_column :characters, :show_id, :integer
     add_column :characters, :catchphrase, :string 
   end
